a3fd92ca7fdb03deffe739b087934cc5fb98a31c
LuizBlume <luiz.fernando.251107@gmail.com>
2026-07-19 22:35:58 -0300

    Add a blank line for improved readability in variaveisBooleanas.java

--- variaveisBooleanas.java
+++ variaveisBooleanas.java
@@ -2,6 +2,7 @@ public class variaveisBooleanas {
     public static void main(String[] args) {
         double nota1 = 9;
         double nota2 = 8;
+        
         if (nota1 > nota2) {
             System.out.println("A nota 1 é maior");
         }
