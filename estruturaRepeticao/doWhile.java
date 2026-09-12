934493c7b363d76f94040f7a195af6f30faa45b1
LuizBlume <luiz.fernando.251107@gmail.com>
2026-09-12 19:08:46 -0300

    Estruturas de repetição finalizadas

---
+++ estruturaRepeticao/doWhile.java
@@ -0,0 +1,11 @@
+package estruturaRepeticao;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+        do {
+            System.out.println(contador);
+            contador++;
+        } while(contador <= 5);
+    }
+}
---
+++ estruturaRepeticao/forRep.java
@@ -0,0 +1,9 @@
+package estruturaRepeticao;
+
+public class forRep {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 5; count++) {
+            System.out.println(count);
+        }
+    }
+}
